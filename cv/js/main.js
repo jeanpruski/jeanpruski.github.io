6591988fe0263c678fc4ab000270feea249a79f0
jeanpruski <jean.pruski@gmail.com>
2019-02-04 21:45:46 +0100

    maj 190204 cv

---
+++ cv/js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+
+    //Lien vers le modal-web
+    $('#weby').click(function () {
+        $('#container').css('display', 'none');
+        $('#modal-container').css('display', 'block');
+    });
+    //Lien vers le modal-logo
+    $('#logoy').click(function () {
+        $('#container').css('display', 'none');
+        $('#modal-container').css('display', 'block');
+
+    });
+    //Lien vers le modal-print
+    $('#printy').click(function () {
+        $('#container').css('display', 'none');
+        $('#modal-container').css('display', 'block');
+
+
+    });
+    //Lien vers le modal-print
+    $('#othery').click(function () {
+        $('#container').css('display', 'none');
+        $('#modal-container').css('display', 'block');
+
+
+    });
+
+    //Lien vers la Main-page
+    $('#modal-container').click(function () {
+        $('#container').css('display', 'block');
+        $('#modal-container').css('display', 'none');
+
+
+    });
+
+
+
+
+
+});
