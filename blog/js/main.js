c59e2d9f9583f0ab3b187340747faff31dbd9df5
jeanpruski <jean.pruski@gmail.com>
2019-02-04 18:49:27 +0100

    maj cv

---
+++ blog/js/main.js
@@ -0,0 +1,11 @@
+console.log('coucou');
+
+$(".over1").click(function () {
+    $(".img1").toggle();
+});
+$(".over2").click(function () {
+    $(".img2").toggle();
+});
+$(".over3").click(function () {
+    $(".img3").toggle();
+});
---
+++ exo/js/main.js
@@ -0,0 +1,5 @@
+$('#mamadou').click(function () {
+    $('#mamadou').css('transform', 'scale(1.6)');
+    console.log('couocu');
+
+});
