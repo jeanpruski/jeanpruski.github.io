ec1ad3abf1d02129ac50ebd021789616d951fb33
jeanpruski <jean.pruski@gmail.com>
2019-03-09 14:51:50 +0100

    firt test

--- cv/js/main.js
+++ cv/js/main.js
@@ -1,38 +1,40 @@
 $(document).ready(function () {
 
-    //Lien vers le modal-web
-    $('#weby').click(function () {
-        $('#container').css('display', 'none');
-        $('#modal-container').css('display', 'block');
-    });
-    //Lien vers le modal-logo
-    $('#logoy').click(function () {
-        $('#container').css('display', 'none');
-        $('#modal-container').css('display', 'block');
 
-    });
-    //Lien vers le modal-print
-    $('#printy').click(function () {
-        $('#container').css('display', 'none');
-        $('#modal-container').css('display', 'block');
+    /*  //Lien vers le modal-web
+      $('#weby').click(function () {
+          $('#container').css('display', 'none');
+          $('#modal-container').css('display', 'block');
+      });
+      /*
+      //Lien vers le modal-logo
+      $('#logoy').click(function () {
+          $('#container').css('display', 'none');
+          $('#modal-container').css('display', 'block');
 
-
-    });
-    //Lien vers le modal-print
-    $('#othery').click(function () {
-        $('#container').css('display', 'none');
-        $('#modal-container').css('display', 'block');
+      });
+      //Lien vers le modal-print
+      $('#printy').click(function () {
+          $('#container').css('display', 'none');
+          $('#modal-container').css('display', 'block');
 
 
-    });
+      });
+      //Lien vers le modal-print
+      $('#othery').click(function () {
+          $('#container').css('display', 'none');
+          $('#modal-container').css('display', 'block');
 
-    //Lien vers la Main-page
-    $('#modal-container').click(function () {
-        $('#container').css('display', 'block');
-        $('#modal-container').css('display', 'none');
+
+      });
+
+      //Lien vers la Main-page
+      $('#modal-container').click(function () {
+          $('#container').css('display', 'block');
+          $('#modal-container').css('display', 'none');
 
 
-    });
+      });*/
 
 
 
